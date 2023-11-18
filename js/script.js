088d6f4c50b7ec21ace80d80a8a69d32508a3956
Jason Warren <contact.jwarren@gmail.com>
2023-11-18 13:20:06 +0000

    form form FORM!

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-import 'dotenv/config';
-
 // HTML ELements
+	// Story
+	const title = document.getElementById("title");
+	const image = document.getElementById("image");
+	const narrative = document.getElementById("narrative");
+	const optionAText = document.getElementById("optionAText");
+	const optionBText = document.getElementById("optionBText");
+	const optionCText = document.getElementById("optionCText");
 
-const title = document.getElementById("title");
-const image = document.getElementById("image");
-const narrative = document.getElementById("narrative");
-const optionAText = document.getElementById("optionAText");
-const optionBText = document.getElementById("optionBText");
-const optionCText = document.getElementById("optionCText");
+//API Key
+	let myKey = '';
+	
+	//Form
+	const apiKeyForm = document.getElementById('apiKey');
+	const apiKeyInput = document.getElementById('inputAPIKey');
+
+	apiKeyForm.addEventListener('submit', function (event) {
+  	event.preventDefault();
+		console.log("My Key: " + myKey);
+		myKey = apiKeyInput.value;
+		apiKeyInput.value = 'Mine Now';
+		console.log("My Key: " + myKey);
+	});
