7d02a190eb9ffc8048486ad520c23e4644877d17
PAZHames <phoebehames292@gmail.com>
2023-11-18 12:45:54 +0000

    accessed html elements through JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 import 'dotenv/config';
+
+// HTML ELements
+
+const title = document.getElementById("title");
+const image = document.getElementById("image");
+const narrative = document.getElementById("narrative");
+const optionAText = document.getElementById("optionAText");
+const optionBText = document.getElementById("optionBText");
+const optionCText = document.getElementById("optionCText");
